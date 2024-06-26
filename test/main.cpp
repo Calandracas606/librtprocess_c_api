7ca0ff72a61573575e8bfe0ad8cc6495722f262c
Daniel Martinez <danielmartinez@cock.li>
2024-06-26 12:31:01 -0400

    add num_iters option to test

--- test/main.cpp
+++ test/main.cpp
@@ -6,69 +6,54 @@
 #include <libraw/libraw.h>
 #include <vector>
 
-int main(void) {
+int main(int argc, char **argv) {
   std::cout << "Hello, World\n";
 
   auto lr = LibRaw{};
   std::string file_name = "mtl.dng";
 
-  std::cout << "test 1\n";
   auto ret = lr.open_file(file_name.c_str());
   if (ret != LIBRAW_SUCCESS) {
     auto libraw_msg = libraw_strerror(ret);
     throw std::runtime_error(libraw_msg);
   }
 
-  std::cout << "test 2\n";
-
   ret = lr.unpack();
-  std::cout << "test 3\n";
   if (ret != LIBRAW_SUCCESS) {
     auto libraw_msg = libraw_strerror(ret);
     throw std::runtime_error(libraw_msg);
   }
 
-  std::cout << "test 4\n";
   auto h = lr.imgdata.sizes.raw_height;
   auto w = lr.imgdata.sizes.raw_width;
 
   // auto num_px = static_cast<std::size_t>(h * w)
-  std::cout << "test 5\n";
   auto rawdata = std::vector<float *>(h, nullptr);
   auto raw_buf = std::vector<float>(h * w, 0.0);
-  std::cout << "test 6\n";
   rawdata[0] = raw_buf.data();
   for (std::size_t i = 1; i < h; ++i)
     rawdata[i] = rawdata[i - 1] + w;
-  std::cout << "test 7\n";
 
   auto red = std::vector<float *>(h, nullptr);
   auto red_buf = std::vector<float>(h * w, 0.0f);
-  std::cout << "test 8\n";
   red[0] = red_buf.data();
   for (std::size_t i = 1; i < h; ++i)
     red[i] = red[i - 1] + w;
-  std::cout << "test 9\n";
 
   auto green = std::vector<float *>(h, nullptr);
   auto green_buf = std::vector<float>(h * w, 0.0f);
-  std::cout << "test 10\n";
   green[0] = green_buf.data();
   for (std::size_t i = 1; i < h; ++i)
     green[i] = green[i - 1] + w;
-  std::cout << "test 11\n";
 
   auto blue = std::vector<float *>(h, nullptr);
   auto blue_buf = std::vector<float>(h * w, 0.0f);
-  std::cout << "test 11\n";
   blue[0] = blue_buf.data();
   for (std::size_t i = 1; i < h; ++i)
     blue[i] = blue[i - 1] + w;
-  std::cout << "test 12\n";
 
   unsigned int cfarray[2][2];
   int max_filter = 0;
-  std::cout << "test 13\n";
   for (int row = 0; row < 2; ++row) {
     for (int col = 0; col < 2; ++col) {
       const int filter = lr.COLOR(row, col);
@@ -77,12 +62,10 @@ int main(void) {
       cfarray[row][col] = filter < 3 ? filter : 1;
     }
   }
-  std::cout << "test 14\n";
 
   for (std::size_t i = 0; i < h * w; ++i) {
     raw_buf[i] = lr.imgdata.rawdata.raw_image[i];
   }
-  std::cout << "test 14\n";
 
   // rcd_demosaic(w, h, rawdata.data(), red.data(), green.data(), blue.data(),
   //              cfarray, [](double progress) {
@@ -90,22 +73,22 @@ int main(void) {
   //                return false;
   //              });
 
-  auto err = amaze_demosaic(
-      w, h, lr.imgdata.sizes.left_margin, lr.imgdata.sizes.top_margin, w, h,
-      rawdata.data(), red.data(), green.data(), blue.data(), cfarray,
-      [](double progress) -> bool {
-        std::cerr << "amaze progress: " << progress << "\n";
-        return false;
-      },
-      1, 0, 1, 1, 2, true);
-
-  std::cout << "test 15\n";
+  auto num_iters = (argc >= 2 ? atoi(argv[1]) : 1);
+  std::cout << "Running " << num_iters << " Iterations... \n";
+  for (int i = 0; i < num_iters; ++i) {
+    auto err = amaze_demosaic(
+        w, h, lr.imgdata.sizes.left_margin, lr.imgdata.sizes.top_margin, w, h,
+        rawdata.data(), red.data(), green.data(), blue.data(), cfarray,
+        [](double progress) -> bool {
+          //       std::cerr << "amaze progress: " << progress << "\n";
+          return false;
+        },
+        1, 0, 1, 1, 2, true);
+  }
 
   float **img[3] = {red.data(), green.data(), blue.data()};
-  std::cout << "test 16\n";
 
   tiff_dump(img, w, h, "out.tiff");
-  std::cout << "test 17\n";
 }
 // RTPROCESS_API rpError amaze_demosaic(int raw_width, int raw_height, int winx,
 // int winy, int winw, int winh, const float * const *rawData, float **red,
